40d6b44ce083c4d82d5beca86da844ac2ca90695
Arek Krakowiak <369arek12@protonmail.com>
2026-03-06 00:16:06 +0100

    add email and password states for login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 import './Login.css';
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <h1>Sign in</h1>
       <p><Link to="/register" className='signUp'>Sign up</Link> if you don't have an account yet.</p>
+      
       <form>
-        <input type="email" placeholder="Your Email" required />
-        <input type="password" placeholder="Your Password" required />
+        <input
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <button type="submit">Sign in</button>
       </form>
+      
       <button className='google'>Sign in with Google</button>
     </div>
   );
