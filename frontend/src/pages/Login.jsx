e072a986918a946b2e465cae6b762d7ee789b1be
Arek Krakowiak <369arek12@protonmail.com>
2026-03-06 00:09:12 +0100

    style upon hover effects

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 export default function Login() {
   return (
     <div>
       <h1>Sign in</h1>
+      <p><Link to="/register" className='signUp'>Sign up</Link> if you don't have an account yet.</p>
       <form>
         <input type="email" placeholder="Your Email" required />
         <input type="password" placeholder="Your Password" required />
