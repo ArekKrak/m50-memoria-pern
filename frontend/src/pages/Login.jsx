3e036b8f9c1717118461f26894f3aa8417dc2c4f
Arek Krakowiak <369arek12@protonmail.com>
2026-03-05 23:41:39 +0100

    add form and buttons to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,12 @@ export default function Login() {
   return (
     <div>
       <h1>Sign in</h1>
+      <form>
+        <input type="email" placeholder="Your Email" required />
+        <input type="password" placeholder="Your Password" required />
+        <button type="submit">Sign in</button>
+      </form>
+      <button>Sign in with Google</button>
     </div>
   );
 }
