6822fdc76d5e8c8bf4c9c9663438566a0a922372
Arek Krakowiak <369arek12@protonmail.com>
2026-03-05 22:28:27 +0100

    wire the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import memoriaLogo from '/banner.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
+import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import CreateNote from './pages/CreateNote';
+import EditNote from './pages/EditNote';
+import memoriaLogo from '/banner.svg';
 
 function App() {
 
   return (
     <>
       <Navbar bannerSrc={memoriaLogo} />
-      <Login />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/notes/new' element={<CreateNote />} />
+        <Route path='/notes/:id/edit' element={<EditNote />} />
+        <Route path='*' element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+/* BrowserRouter enables client-side routing. It allows to manage navigation without triggering full page reloads. */
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
