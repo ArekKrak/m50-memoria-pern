7718f812216094db869b6119b283b81bbc57857b
Arek Krakowiak <369arek12@protonmail.com>
2026-03-05 22:51:45 +0100

    import Link and add clickable elements

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import './Navbar.css'
+import './Navbar.css';
+import { Link } from 'react-router-dom'; // Adding clickability so the elements supposed to redirect actually do it
 
 export default function Navbar({ bannerSrc }) {
   return (
     <header>
       <img src={bannerSrc} alt="Memoria banner" width="900" height="300" />
-      <h2 className="brand">memoria</h2>
+      <h2 className="brand">
+        <Link to="/" className='brandLink'>memoria</Link>
+      </h2>
+      <nav className='nav'>
+        <Link to="/login">Sign in</Link>
+        <Link tp="/register">Sign up</Link>
+      </nav>
     </header>
   );
 }
