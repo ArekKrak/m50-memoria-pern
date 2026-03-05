8e2b6d9c726831d222978a54b8195cb47420dfae
Arek Krakowiak <369arek12@protonmail.com>
2026-03-05 19:52:02 +0100

    apply cosmetic changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import './Navbar.css'
+
 export default function Navbar({ bannerSrc }) {
   return (
     <header>
       <img src={bannerSrc} alt="Memoria banner" width="800" height="250" />
-      <h2>memoria</h2>
+      <h2 className="brand">memoria</h2>
     </header>
   );
 }
