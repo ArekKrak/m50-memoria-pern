1e075660e1ebe9e1712b8711fb1085d385eb8739
Arek Krakowiak <369arek12@protonmail.com>
2026-03-06 18:43:23 +0100

    add psql query to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const pool = require("./db");
 const express = require("express");
 
 const app = express();
@@ -9,6 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Memoria API running");
 });
+
+pool.query("SELECT NOW()")
+  .then((res) => console.log("Database connected:", res.rows[0]))
+  .catch((err) => console.error("Database connection error:", err));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
