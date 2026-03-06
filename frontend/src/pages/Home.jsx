cd33f09e359b9dd11e87d20b58693a61c66e0e37
Arek Krakowiak <369arek12@protonmail.com>
2026-03-07 00:53:41 +0100

    edit navbar, home, and banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import './Home.css';
-import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className='welcomeText'>
       <h1>Welcome to memoria.</h1>
-      <nav className='nav'>
-        <Link to="/login">Sign in</Link>
-        <Link to="/register">Sign up</Link>
-      </nav>
+      <p>Your personal knowledge base</p>
     </div>
   );
 }
