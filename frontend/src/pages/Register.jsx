9fbe8373749e8a50d0449235ae8dffef78f58730
Arek Krakowiak <369arek12@protonmail.com>
2026-03-06 01:13:20 +0100

    implement and style register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Register.css";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify({ email, password })
+      });
+
+      if (res.ok) {
+        navigate("/dashboard");
+      } else {
+        setError("Registration failed.");
+        setPassword("");
+      }
+    } catch (err) {
+      console.error("Network error", err);
+    }
+  };
+
   return (
     <div>
       <h1>Sign up</h1>
+      <p>Already signed up? <Link to="/login" className="signIn"> Sign in here</Link></p>
+
+      <form onSubmit={{handleSubmit}}>
+        <input
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign up</button>
+      </form>
+      {error && <p className="error">{error}</p>}
     </div>
   );
 }
